e8979efd73723f834119284ccf979fda27de2675
Savio Andrade <saandrade@dundee.ac.uk>
2019-04-29 09:42:51 +0100

    changed video download from pytube to YouTube-dl

--- misc_func.py
+++ misc_func.py
@@ -4,6 +4,27 @@ import os
 import numpy as np
 from shutil import copyfile, rmtree, move
 import platform
+import re
+import subprocess
+import youtube_dl
+import os
+import sys
+import warnings
+
+warnings.filterwarnings("ignore")
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+# f = open(os.devnull, 'w')
+# sys.stdout = f
 
 def getMaxVolume(s):
     maxv = float(np.max(s))
@@ -11,9 +32,27 @@ def getMaxVolume(s):
     return max(maxv,-minv)
 
 def downloadFile(url):
-    sleep(10)
-    name = YouTube(url).streams.first().download()
-    return name
+
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s', 'logger':MyLogger(),})
+    with ydl:
+        result = ydl.extract_info(
+            url,
+            download=True
+            )
+
+    inputname = ''.join([result['id'], result['ext'], '.', result['ext']])
+    alt_inputname = ''.join([result['id'], result['ext'], '.', 'mkv'])
+    title = result['title']
+    title = "".join([x if x not in "\/:*?<>|" else " " for x in title])
+    exists = os.path.isfile(inputname)
+
+    if exists:
+        format = result['ext']
+    else:
+        inputname = alt_inputname
+        format = 'mkv'
+    #name = YouTube(url).streams.first().download()
+    return inputname, title, format
 
 def copyFrame(inputFrame,outputFrame, TEMP_FOLDER):
     src = TEMP_FOLDER+"/frame{:06d}".format(inputFrame+1)+".jpg"
@@ -37,10 +76,17 @@ def deletePath(s): # Dangerous! Watch out!
     except OSError:
         print ("Deletion of the directory %s failed" % s)
         print(OSError)
-        
+
 def fix_input(input):
     if platform.system() == 'Linux':
         input = input.split("/")
     else:
         input = input.split("\\")
     return input[-1]
+
+def getFrameRate(path):
+    process = subprocess.Popen(["ffmpeg", "-i", path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    stdout, _ = process.communicate()
+    output =  stdout.decode()
+    match_dict = re.search(r"\s(?P<fps>[\d\.]+?)\stbr", output).groupdict()
+    return float(match_dict["fps"])
